e2454ad6dafb59e13c1e7e47505e0032c7fb80e5
Rodrigo Toste Gomes <rgomes@mit.edu>
2014-04-10 14:15:29 -0400

    Added new algorithm: combination of previous algorithms; improved test method; output of tests.

---
+++ pythonLVA/combinationLearner.py
@@ -0,0 +1,21 @@
+import partitionHmmLearner
+import simpleLearner
+
+learners = [
+    partitionHmmLearner.HMMLearner(2),
+    simpleLearner.SimpleLearner()]
+alphas = [0.5,0.5]
+
+class CombinationLearner:
+    def __init__(self):
+        self.name = 'Combination Learner: ' + ','.join([l.name for l in learners])
+
+    def learn(self, data):
+        for l in learners:
+            l.learn(data)
+
+    def predict(self, user, period):
+        prediction = 0
+        for a,l in zip(alphas,learners):
+            prediction += a*l.predict(self, user, period)
+        return prediction
